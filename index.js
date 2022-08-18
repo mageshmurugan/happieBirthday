ec77e308bef2154a22ba2c4bc464473f887110cb
Magesh <mageshmurugan64@gmail.com>
2022-08-18 21:04:42 +0530

    add a script

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ app.post('/', async (req, res) => {
 setInterval(myFunction, 1000)
 
 async function myFunction() {
+    let d = new Date();
     const tim = d.getHours() + ':' + d.getMinutes()
     console.log(tim)
 
