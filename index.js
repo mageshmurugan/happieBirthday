8b82df38bb7e77dc0a3da83f664db32aa4e4947c
Magesh <mageshmurugan64@gmail.com>
2022-08-20 09:19:48 +0530

    add a script

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 
-// const dbUrl = 'mongodb://localhost:27017/dates';
-const dbUrl = 'mongodb+srv://happybirthdaymessage:woV76BMccfoLYRjI@cluster1.ejllaob.mongodb.net/?retryWrites=true&w=majority'
+const dbUrl = 'mongodb://localhost:27017/dates';
+// const dbUrl = 'mongodb+srv://happybirthdaymessage:woV76BMccfoLYRjI@cluster1.ejllaob.mongodb.net/?retryWrites=true&w=majority'
 // const dbUrl = process.env.DB_URL
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -56,53 +56,40 @@ app.post('/', async (req, res) => {
     // console.log(n)
     const tit = n.join('-');
 
-    const a = tit.split('-');
-    const b = a[0].charAt(0);
-    if (b == '0') {
-        a[0] = a[0].slice(1);
-        // console.log(a)
-    }
-    const c = a[1].charAt(0);
-    if (c == '0') {
-        a[1] = a[1].slice(1);
-    }
-    const da = a.join(':');
+    // const a = tit.split('-');
+    // const b = a[0].charAt(0);
+    // if (b == '0') {
+    //     a[0] = a[0].slice(1);
+    //     // console.log(a)
+    // }
+    // const c = a[1].charAt(0);
+    // if (c == '0') {
+    //     a[1] = a[1].slice(1);
+    // }
+    // const da = a.join(':');
     // console.log(da)
 
-    const data = new Dates({ email: email, names: names, date: da, year: sp });
+    const data = new Dates({ email: email, names: names, date: tit, year: sp });
     await data.save();
     console.log(data)
     res.redirect('/')
 })
 
 
-setInterval(myFunction, 1000)
+setInterval(myFunction, 1000 * 60 * 10)
 
 async function myFunction() {
     const date = new Date()
     // const timeZone = 'Asia/Kolkata';
     // const tim = d.getHours() + ':' + d.getMinutes()
-    const formatters = [new Intl.DateTimeFormat('sv', { dateStyle: 'short', timeZone: 'Asia/Kolkata' })]
+    const formatters = new Intl.DateTimeFormat('sv', { dateStyle: 'short', timeZone: 'Asia/Kolkata' })
     // const formatters = [new Intl.DateTimeFormat('sv', { timeZone: 'Asia/Kolkata' })]
-    formatters.forEach(fmt => console.log(fmt.format(date).split('-')))
+    // formatters.forEach(fmt => console.log(fmt.format(date).split('-')))
+    const hell = formatters.format(date).split('-').slice(1).join('-');
     // const a = hell.split('-');
-    // console.log(a)
-
-    // let d = new Date().toLocaleString("en-us", { timeZone: 'Asia/kolkata' });
-    // const gim = d.getMonth() + ':' + d.getDate()
-    // console.log(tim)
-    // console.log(gim)
-
-}
-
-
-
-const textJob = new cronJob('1 1 1 * * *', async function () {
-    // const textJob = new cronJob('1 * * * * *', async function () {
-    let d = new Date();
-    const dat = d.getMonth() + 1 + ':' + d.getDate();
+    console.log(hell)
     const findDate = await Dates.find({
-        date: dat
+        date: hell
     });
     if (findDate) {
         for (let datq of findDate) {
@@ -130,12 +117,52 @@ const textJob = new cronJob('1 1 1 * * *', async function () {
         }
 
     }
-}, null, true);
+
+
+
+}
+
+
+
+// const textJob = new cronJob('1 1 1 * * *', async function () {
+//     // const textJob = new cronJob('1 * * * * *', async function () {
+//     let d = new Date();
+//     const dat = d.getMonth() + 1 + ':' + d.getDate();
+//     const findDate = await Dates.find({
+//         date: dat
+//     });
+//     if (findDate) {
+//         for (let datq of findDate) {
+//             const mailOptions = {
+//                 from: 'Happy Birthday <mageshmurugan64@gmail.com>',
+//                 to: `${datq.names} <${datq.email}>`,
+//                 subject: `Happy Birthday ${datq.names}`,
+//                 text: `Wishing You the Best Birthday ${datq.names} `
+//                 // html: `<div>
+//                 // <h1>hello${datq.names}</h1>
+//                 // </div>`
+
+//             };
+//             authmail.sendMail(mailOptions,
+//                 function (error, info) {
+//                     if (error) {
+//                         console.log('ERROR')
+//                         console.log(error);
+//                     } else {
+//                         console.log('Email Sent :' + info.response);
+//                     }
+//                 });
+//             // console.log(datq.year)
+//             // console.log(d.getFullYear() - datq.year)
+//         }
+
+//     }
+// }, null, true);
 
 // happybirthdaymessage
 // woV76BMccfoLYRjI
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`SERVING ON PORT ${port}`)
 })
